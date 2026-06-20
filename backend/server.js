f388bf839d3243925e9f4b3aa88e182c59f3b33c
Sanat <sanatbaral64@gmail.com>
2026-06-21 00:26:40 +0530

    chore: prepare backend cors for production

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,45 @@ require("./sheets");
 const app = express();
 
 
+const allowedOrigins = [
+
+"http://localhost:5173",
+
+"http://localhost:5174",
+
+process.env.FRONTEND_URL
+
+];
+
+
 app.use(cors({
 
-origin:"http://localhost:5173",
+origin:function(origin,callback){
+
+
+if(
+!origin ||
+allowedOrigins.includes(origin)
+){
+
+callback(null,true);
+
+}
+
+else{
+
+callback(
+new Error("Not allowed by CORS")
+);
+
+}
+
+
+},
+
 
 credentials:true
+
 
 }));
 
